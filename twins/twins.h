a49702041af532f988487fb8abe4125e5c104dca
Ffawka <miss94961@gmail.com>
2026-03-19 18:57:48 +0300

    Create twins.h

---
+++ twins/twins.h
@@ -0,0 +1,8 @@
+#ifndef TWINS_H
+#define TWINS_H
+
+int isPrime(int n);
+void findTwinPrimes(int limit, const char *filename);
+void findTwinPrimesFromFile(int limit, const char *outputFile);
+
+#endif
